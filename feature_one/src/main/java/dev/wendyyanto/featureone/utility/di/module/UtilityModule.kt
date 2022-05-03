0e7a4429ae048438af006f1658fdc60a1dc5717f
wendyyanto <yanto.wendy98@gmail.com>
2022-05-03 23:08:46 +0700

    feature: setup feature_one api for featureOneUtility

--- feature_one/src/main/java/dev/wendyyanto/featureone/utility/di/module/UtilityModule.kt
+++ feature_one/src/main/java/dev/wendyyanto/featureone/utility/di/module/UtilityModule.kt
@@ -1,16 +1,15 @@
 package dev.wendyyanto.featureone.utility.di.module
 
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
-import dev.wendyyanto.base.utility.AppUtility
-import dev.wendyyanto.featureone.utility.FeatureOneUtility
+import dev.wendyyanto.feature_one_api.FeatureOneUtility
+import dev.wendyyanto.featureone.utility.FeatureOneUtilityImpl
 import dev.wendyyanto.featureone.utility.di.scope.FeatureOneScope
 
 @Module
-object UtilityModule {
+interface UtilityModule {
 
   @FeatureOneScope
-  @Provides
-  fun provideFeatureOneUtility(appUtility: AppUtility): FeatureOneUtility =
-    FeatureOneUtility(appUtility)
+  @Binds
+  fun bindsFeatureOneUtility(featureOneUtility: FeatureOneUtilityImpl): FeatureOneUtility
 }
