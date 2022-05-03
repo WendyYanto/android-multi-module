37c937ebbc76371faffd79e433a4c4a5cad6e25f
wendyyanto <yanto.wendy98@gmail.com>
2022-05-03 22:58:52 +0700

    feature: clean up app module

--- app/src/main/java/dev/wendyyanto/androidmultimodule/App.kt
+++ app/src/main/java/dev/wendyyanto/androidmultimodule/App.kt
@@ -4,8 +4,6 @@ import android.app.Application
 import dev.wendyyanto.androidmultimodule.di.ComponentProvider
 import dev.wendyyanto.androidmultimodule.di.module.AppComponent
 import dev.wendyyanto.androidmultimodule.di.module.DaggerAppComponent
-import dev.wendyyanto.featureone.utility.di.component.FeatureOneComponent
-import dev.wendyyanto.featuretwo.utility.di.component.FeatureTwoComponent
 
 class App : Application(), ComponentProvider {
 
@@ -17,13 +15,5 @@ class App : Application(), ComponentProvider {
 
   override fun loadAppComponent(): AppComponent {
     return appComponent
-  }
-
-  override fun loadFeatureOneComponent(): FeatureOneComponent {
-    return appComponent.featureOneComponent()
-  }
-
-  override fun loadFeatureTwoComponent(): FeatureTwoComponent {
-    return appComponent.featureTwoComponent()
   }
 }
--- app/src/main/java/dev/wendyyanto/androidmultimodule/di/ComponentProvider.kt
+++ app/src/main/java/dev/wendyyanto/androidmultimodule/di/ComponentProvider.kt
@@ -2,9 +2,17 @@ package dev.wendyyanto.androidmultimodule.di
 
 import dev.wendyyanto.androidmultimodule.di.module.AppComponent
 import dev.wendyyanto.featureone.utility.di.FeatureOneComponentProvider
+import dev.wendyyanto.featureone.utility.di.component.FeatureOneComponent
 import dev.wendyyanto.featuretwo.utility.di.FeatureTwoComponentProvider
+import dev.wendyyanto.featuretwo.utility.di.component.FeatureTwoComponent
 
 interface ComponentProvider : FeatureOneComponentProvider, FeatureTwoComponentProvider {
 
   fun loadAppComponent(): AppComponent
+
+  override fun loadFeatureOneComponent(): FeatureOneComponent =
+    loadAppComponent().featureOneComponent()
+
+  override fun loadFeatureTwoComponent(): FeatureTwoComponent =
+    loadAppComponent().featureTwoComponent()
 }
